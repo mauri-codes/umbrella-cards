60b17f4f9a358f1a802f456fdbdd0717c5e331a1
mauri1789 <mauricio.alarcon1789@gmail.com>
2017-01-17 09:30:34 -0400

    Styles added, form validation improved

--- client/app/User.ts
+++ client/app/User.ts
@@ -4,6 +4,8 @@ export class User {
   email: string;
   password: string;
   displayName: string;
+  createdAt: string;
+  validated: string;
   constructor(us: string, el: string, rl: string){
     this.username       = us;
     this.email          = el;
--- client/app/user-list.component.ts
+++ client/app/user-list.component.ts
@@ -5,12 +5,27 @@ import { User }           from './User';
 @Component({
   moduleId: module.id,
   selector: 'user-list',
-  templateUrl: './html/user-list.component.html'
+  templateUrl: './html/user-list.component.html',
+  styleUrls: ['styles/tables.css', 'styles/styles.css']
 })
 
 export class UserListComponent{
   users: User[];
   constructor(userService: UserService){
-    userService.getUsers().subscribe(users => this.users = users, err => console.log (err) );
+    userService.getUsers().subscribe(users => {this.users = users; this.convert();}, err => console.log (err) );
+  }
+  deleteUser(){
+    window.location.href = "/";
+  }
+  deleteUser2(){
+    window.location.href = "/users";
+  }
+  //convert the date to something more readable
+  convert(){
+    this.users.forEach(function (e) {
+      var date = new Date(e.createdAt);
+      e.createdAt = (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear();
+    });
   }
 }
+
